6aa07be62cd1d671e2cbbef6af9de86008ee3ac1
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-18 00:47:28 +0700

    update: navbar

--- src/components/layouts/navbar/navbar.tsx
+++ src/components/layouts/navbar/navbar.tsx
@@ -1,27 +1,77 @@
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
 export default function Navbar() {
-    return(
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+  return (
+    <nav className={`navbar navbar-expand-lg p-2 fixed-top ${styles["nav-box"]}`}>
+      <div
+        className="container"
+        style={{
+          fontSize: "28px",
+        }}
+      >
+        <div
+          className="navbar-brand"
+          style={{
+            fontSize: "25px",
+          }}
+        >
+          Over
+          <span
+            style={{
+              color: "#ff9c00",
+            }}
+          >
+            Logic
+          </span>
+        </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+          style={{
+            marginLeft: "100px",
+          }}
+        >
+          <ul className="navbar-nav gap-5 fw-semibold">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" href="#">
+                <div className={`${styles["li-navbar"]}`}>Home</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                <div className={`${styles["li-navbar"]}`}>Shop Online</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                <div className={`${styles["li-navbar"]}`}>What's New</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                <div className={`${styles["li-navbar"]}`}>Contact</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                <div className={`${styles["li-navbar"]}`}>About Us</div>
+              </Link>
+            </li>
+            <div className="d-flex ms-4">
+              <Link className={`nav-link ${styles["sign-up-box"]}`} href="#">
+                Sign Up
+              </Link>
+              <Link className={`nav-link ${styles["sign-in-box"]} ms-2`} href="#">
+                Sign In
+              </Link>
+            </div>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>);
+    </nav>
+  );
 }
