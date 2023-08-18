1b6699ba5b2c0d9a200f81f0265adb2824d0ba35
Joko Supriyanto <jokosupriyanto1712@gmail.com>
2023-08-18 23:17:44 +0700

    create tranding card product

---
+++ src/components/elements/tranding/tranding.tsx
@@ -0,0 +1,18 @@
+import styles from './tranding.module.css'
+
+export default function Tranding() {
+    return (
+        <>
+            <div className={`${styles["tranding-container"]}`}>
+                <div className={`${styles["tranding-top"]}`}>
+                </div>
+                <div className={`${styles["tranding-bot"]}`}>
+                    <div>
+                        <h4>Product Category</h4>
+                    </div>
+                    <div className={`btn rounded-pill ${styles["tranding-button"]}`}>Shop Now</div>
+                </div>
+            </div>
+        </>
+    )
+}
