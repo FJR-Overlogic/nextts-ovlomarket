c0babeb5e3bdca36a4fe62b314d6160b7e81b651
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-18 15:25:10 +0700

    update: header view to be responsive

--- src/components/elements/header/header.tsx
+++ src/components/elements/header/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className={`${styles["header"]}`}>
       <div className={`${styles["image-container"]}`}>
-        <Image src={HeaderImage} alt="header" />
+        <Image src={HeaderImage} className={`${styles["header-img"]}`} alt="header" priority />
         <div className={` text-start ${styles["title-header"]}`}>
           <div>
             Find The Best Products
@@ -20,15 +20,12 @@ export default function Header() {
               Realiability
             </span>
           </div>
-          <div>
-            <div className={`input-group mb-3 ${styles["input-box"]}`}>
-              <input type="text" className={`form-control ${styles["input"]}`} placeholder="Search Here Or Shop Product" aria-label="Search Here Or Shop Product" aria-describedby="button-addon2" />
-              <button className={`btn ${styles["button"]}`} type="button" id="button-addon2">
-                Search
-              </button>
-            </div>
-          </div>
+          <div></div>
         </div>
+        <form action="" className={`mb-3 ${styles["search-box"]}`}>
+          <input type="text" placeholder="Search Here Or Shop Product" />
+          <button type="submit">Search</button>
+        </form>
       </div>
     </div>
   );
