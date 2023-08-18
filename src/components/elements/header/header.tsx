93091bd83a56494a510aced9b771ba7f82eabf25
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-18 16:47:50 +0700

    update: header view

--- src/components/elements/header/header.tsx
+++ src/components/elements/header/header.tsx
@@ -1,31 +1,33 @@
+"use client";
 import Image from "next/image";
 import HeaderImage from "@/assets/header.png";
 import styles from "./header.module.css";
+import Lottie from "lottie-react";
+import LottieHeader from "@/assets/lottie/LottieHeader.json";
 
 export default function Header() {
   return (
     <div className={`${styles["header"]}`}>
-      <div className={`${styles["image-container"]}`}>
-        <Image src={HeaderImage} className={`${styles["header-img"]}`} alt="header" priority />
-        <div className={` text-start ${styles["title-header"]}`}>
-          <div>
-            Find The Best Products
-            <br /> With
+      <div className={`${styles["header-container"]}`}>
+        <Lottie animationData={LottieHeader} className={`${styles["lottie-header"]}`} autoPlay={true} loop={true} data-aos="fade-right" />
+        <div className={`${styles["title-input-box"]}`} data-aos="fade-left">
+          <div className={`${styles["title-header"]}`}>
+            Welcome To <br />
+            Over
             <span
               style={{
                 color: "#ff9c00",
               }}
             >
-              {" "}
-              Realiability
+              Logic{" "}
             </span>
+            Market
           </div>
-          <div></div>
+          <form action="" className={`mb-3 ${styles["search-box"]}`}>
+            <input type="text" placeholder="Search Here Or Shop Product" />
+            <button type="submit">Search</button>
+          </form>
         </div>
-        <form action="" className={`mb-3 ${styles["search-box"]}`}>
-          <input type="text" placeholder="Search Here Or Shop Product" />
-          <button type="submit">Search</button>
-        </form>
       </div>
     </div>
   );
