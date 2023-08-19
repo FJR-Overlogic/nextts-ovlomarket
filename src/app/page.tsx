08ea1e8fb2018f27a3fb9631748ec984aad8a075
mfl4 <mhdfarhanl6267@gmail.com>
2023-08-20 00:53:33 +0700

    refactor: delete unused import on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css';
-import Header from '@/components/elements/header/header';
-import Footer from '@/components/elements/footer/footer';
+import Header from "@/components/elements/header/header";
+import Footer from "@/components/elements/footer/footer";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Footer />
     </div>
-   );
-  }
+  );
+}
