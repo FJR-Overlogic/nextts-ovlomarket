dc8b0f4984e9edac69d33a9f005a133555bc746f
Joko Supriyanto <114573843+Josumaru@users.noreply.github.com>
2023-08-20 21:51:22 +0700

    Update product.tsx

--- src/components/elements/product/product.tsx
+++ src/components/elements/product/product.tsx
@@ -1,10 +1,11 @@
+import styles from "./product.module.css"
+
 export default function Product() {
     return (
         <>
-            <div style={{ borderRadius: "4px", width: "300px", height:"300px" }} className="container">
-                <div className="rounded" style={{ paddingBottom: "10px", width: "100%" , height:"100%", overflow:"hidden"}}>
+            <div style={{ borderRadius: "4px", width: "300px", height:"500px" }} className="container">
+                <div className="rounded" style={{ paddingBottom: "10px", width: "100%" , height:"60%", overflow:"hidden"}}>
                     <img style={{ maxWidth: "100%", maxHeight:"auto", width:"auto", height:"auto" }} src="https://pbs.twimg.com/media/F0GdMsaaYAAjYoO.jpg" alt="product" />
-                    {/* <img className="rounded" style={{ maxWidth: "100%", maxHeight:"100%", display:"block", width:"auto", height:"auto" }} src="https://pbs.twimg.com/media/F0GdMsaaYAAjYoO.jpg" alt="product" /> */}
                 </div>
                 <div>
                     <h5 className="fw-bold">
@@ -13,9 +14,11 @@ export default function Product() {
                     <h5 className="fw-bold">
                         RS.100,000.00
                     </h5>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
-                    </p>
+                    <div className={`${styles.product_desc}`}>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+                        </p>
+                    </div>
                 </div>
                 <div style={{ color: "#FFF", backgroundColor: "#FF9C00", borderRadius: "20px" }} className="btn">Add to cart</div>
             </div>
