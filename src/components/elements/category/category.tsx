bdb29989480b38a1c8a0d6daf5c00ed15d75844b
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-20 18:55:03 +0700

    Fixed: Image on category

--- src/components/elements/category/category.tsx
+++ src/components/elements/category/category.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
+import Laptop from "@/assets/images/laptop.png";
 
 export default function Category() {
   return (
-    <div
-      className="bg-warning text-center rounded"
-      style={{ width: "168px", height: "232px", padding: "1rem" }}
-    >
+    <div className="bg-warning text-center rounded" style={{ width: "168px", height: "232px", padding: "1rem" }}>
       <h5 className="text-black fw-semibold">Tech</h5>
-      <Image src="/laptop.png" width={500} height={500} alt="laptop" />
+      <Image src={Laptop} width={500} height={500} alt="laptop" />
     </div>
   );
 }
