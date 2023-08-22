edd1195549881be1ee00d3e956b26bb644fc2046
Josumaru <jokosupriyanto1712@gmail.com>
2023-08-22 22:27:02 +0700

    setting up tranding components

--- src/components/elements/tranding/tranding.tsx
+++ src/components/elements/tranding/tranding.tsx
@@ -1,18 +1,34 @@
+"use client"
+import { useEffect, useState } from 'react'
 import styles from './tranding.module.css'
 
 export default function Tranding() {
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("Hmm")
+            })
+    }, [])
     return (
-        <>
-            <div className={`${styles["tranding-container"]}`}>
-                <div className={`${styles["tranding-top"]}`}>
+        <div className={`${styles.tranding_section}`}>
+            <div className={`${styles.tranding_subsection}`}>
+                <div className={`${styles.tranding_headers}`}>
+                    <h3>Tranding Products</h3>
                 </div>
-                <div className={`${styles["tranding-bot"]}`}>
-                    <div>
-                        <h4>Product Category</h4>
+                <div className={`${styles.tranding_grid}`}>
+                    <div className={`${styles["tranding-container"]}`}>
+                        <div className={`${styles["tranding-top"]}`}>
+                        </div>
+                        <div className={`${styles["tranding-bot"]}`}>
+                            <div>
+                                <h4>Product Category</h4>
+                            </div>
+                            <div className={`rounded-pill ${styles["tranding-button"]}`}>Shop Now</div>
+                        </div>
                     </div>
-                    <div className={`rounded-pill ${styles["tranding-button"]}`}>Shop Now</div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
