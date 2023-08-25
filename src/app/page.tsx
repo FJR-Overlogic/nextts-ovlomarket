4c497e36b4d16253fdebc317a86528fec5c2a531
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-25 23:18:51 +0700

    [FIXED] error on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Header from "@/components/elements/header/header";
-import Footer from "@/components/elements/footer/footer";
-import Product from "@/components/elements/product/product";
+import Header from "@/components/elements/home/header/header";
+import Footer from "@/components/layouts/footer/footer";
+import Product from "@/components/elements/home/product/product";
+import Tranding from "@/components/elements/home/tranding/tranding";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Product />
+      <Tranding />
       <Footer />
     </div>
   );
