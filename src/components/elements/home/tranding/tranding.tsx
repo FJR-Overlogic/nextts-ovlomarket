34a81a7bb69cecd1268a00172339f5a773978685
Josumaru <jokosupriyanto1712@gmail.com>
2023-08-25 20:27:56 +0700

    add tranding element to home page w/o images

--- src/components/elements/home/tranding/tranding.tsx
+++ src/components/elements/home/tranding/tranding.tsx
@@ -3,32 +3,40 @@ import { useEffect, useState } from 'react'
 import styles from './tranding.module.css'
 
 export default function Tranding() {
+    const [trandingProduct, setTrandingProduct] = useState([]);
     useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
+        fetch("https://fakestoreapi.com/products/categories")
             .then((res) => res.json())
             .then((data) => {
-                console.log("Hmm")
+                setTrandingProduct(data)
             })
     }, [])
     return (
-        <div className={`${styles.tranding_section}`}>
-            <div className={`${styles.tranding_subsection}`}>
-                <div className={`${styles.tranding_headers}`}>
-                    <h3>Tranding Products</h3>
-                </div>
-                <div className={`${styles.tranding_grid}`}>
-                    <div className={`${styles["tranding-container"]}`}>
-                        <div className={`${styles["tranding-top"]}`}>
-                        </div>
-                        <div className={`${styles["tranding-bot"]}`}>
-                            <div>
-                                <h4>Product Category</h4>
+        <div className={`${styles.tranding}`}>
+            {trandingProduct.map((tranding: any, index) => {
+                return (
+                    <div key={index} className={`${styles.tranding_section}`}>
+                        <div className={`${styles.tranding_subsection}`}>
+                            <div className={`${styles.tranding_headers}`}>
+                                <h3 style={{height:"40px"}}>{index==0 && "Tranding Product"}</h3>
                             </div>
-                            <div className={`rounded-pill ${styles["tranding-button"]}`}>Shop Now</div>
+                            <div className={`${styles.tranding_grid}`}>
+                                <div className={`${styles["tranding-container"]}`}>
+                                    <div className={`${styles["tranding-top"]}`}>
+                                    </div>
+                                    <div className={`${styles["tranding-bot"]}`}>
+                                        <div>
+                                            <h4>{tranding.charAt(0).toUpperCase() + tranding.slice(1)}</h4>
+                                        </div>
+                                        <div className={`rounded-pill ${styles["tranding-button"]}`}>Shop Now</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                )
+            })
+            }
         </div>
     )
 }
