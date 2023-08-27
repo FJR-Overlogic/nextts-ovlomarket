0e0858c2b3a5632221c66bbfe115f339d1e13913
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-27 20:45:46 +0700

    [UPDATE] Colors on footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/elements/home/header/header";
 import Footer from "@/components/layouts/footer/footer";
 import Tranding from "@/components/elements/home/tranding/tranding";
+import Product from "@/components/elements/home/product/product";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Product />
       <Tranding />
       <Footer />
     </div>
